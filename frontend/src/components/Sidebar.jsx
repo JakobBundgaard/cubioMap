1a327f0eb32a52af50d4a66e7bc563fc9f432df6
JakobBundgaard <jakobbundgaard@hotmail.com>
2024-11-07 11:27:56 +0100

    Removed Settings icon

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { BsPencilSquare } from "react-icons/bs";
 import { TbPointerPlus } from "react-icons/tb";
-import { IoSettingsOutline } from "react-icons/io5";
+// import { IoSettingsOutline } from "react-icons/io5";
 
 function Sidebar({ selectedArea, isMultiSelectActive, setIsMultiSelectActive, isDrawActive, setIsDrawActive }) {
   return (
@@ -22,9 +22,9 @@ function Sidebar({ selectedArea, isMultiSelectActive, setIsMultiSelectActive, is
             >
                 <TbPointerPlus size={20} />
             </div>
-            <div title="Indstillinger" className="cursor-pointer hover:text-blue-500">
+            {/* <div title="Indstillinger" className="cursor-pointer hover:text-blue-500">
                 <IoSettingsOutline size={20} />
-            </div>
+            </div> */}
         </div>
 
       <h2 className="text-lg font-bold mb-4">Detaljeret Information</h2>
